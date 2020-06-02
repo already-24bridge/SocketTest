5bc994c09aea1c21f361f29783c076b11d230e9c
already-24bridge <52538176+already-24bridge@users.noreply.github.com>
2020-06-02 22:35:43 +0800

    Update client.cpp
    为每个客户端设置一个名字

--- client.cpp
+++ client.cpp
@@ -7,86 +7,141 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <sys/shm.h>
+#include <pthread.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#include <list>
+#include <map>
+
 using namespace std;
-#define BUFFER_SIZE 1024
+
+int sockfd;
+socklen_t len;
+struct sockaddr_in serv_addr;
+std::list<int> li; // list 用来存放套接字 这样可以连接多个client
+std::list<int> rli; // 移除连接的client
+std::map<int, string> tNameMap; // [clientfd] = name
+
+void* getConn(void* arg){
+	while(1){
+		int conn = accept(sockfd, (struct sockaddr*)&serv_addr, &len);
+		li.push_back(conn);
+		// printf("conn %d\n", conn);
+		char sName[255];
+		int n = recv(conn, sName, sizeof(sName)-1, 0);
+		string sTemp = sName;
+		sTemp.erase(sTemp.end() -1); // 删除最后一个换行符
+		tNameMap[conn] = sTemp;
+		printf("conn ect %d name = %s\n",conn, sTemp.c_str());
+	}
+}
+
+void* recvData(void* arg){
+	struct timeval tv;
+	tv.tv_sec = 15;
+	tv.tv_usec = 0;
+	while(1){
+		std::list<int>::iterator it;
+		for (it=li.begin(); it!=li.end(); ++it){
+			fd_set rfds;
+			FD_ZERO(&rfds);
+			int maxfd = 0;
+			int retval = 0;
+			FD_SET(*it, &rfds);
+			if (maxfd < *it){
+				maxfd = *it;
+			}
+			retval = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+			if(retval == -1){
+				printf("select errror\n" );
+			}else if(retval == 0) {
+				// printf("no message waiting\n");
+			}else {
+				char buff[1024];
+				bzero(buff, sizeof(buff)); // memset(buff, 0, sizeof(buff));
+				int len = recv(*it, buff, sizeof(buff)-1, 0);
+				char byeBuff[] = "bye";
+				if (len > 0){
+					printf("recv from[%s] mes = %s \n", tNameMap[*it].c_str(), buff );
+					if (strncmp(buff, byeBuff, 3) == 0 ){ // 这里不使用 strcmp是因为buff的长度是1024 最后是'bye\0' 跟发送来的bye比较值不等 
+						printf("recv %d req close\n", *it);
+						rli.push_back(*it);
+					}
+				}
+			}
+		}
+		for (it = rli.begin(); it != rli.end(); ++it){
+			char buff[] = "bye";
+			printf("send %d bye\n", *it );
+			send(*it, buff, sizeof(buff), 0);
+			li.remove(*it);
+			tNameMap.erase(*it);
+			close(*it);
+		}
+		rli.clear();
+		sleep(1);
+	}
+}
+
+void* sendMess(void* arg){
+	while(1){
+		char buf[1024];
+		fgets(buf, sizeof(buf), stdin);
+		std::list<int>::iterator it;
+		for (it=li.begin(); it!=li.end(); ++it){
+			send(*it, buf, sizeof(buf), 0);
+			printf("send %d msg:%s\n", *it, buf);
+		}
+	}
+}
 
 int main()
 {	
-	fd_set rfds;
-    struct timeval tv;
-    int retval, maxfd;
-
+	// scoket准备
 	const char* ip = "127.0.0.1";
 	int port = 7766;
-	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	int sock_cli = sockfd;
+	sockfd = socket(PF_INET, SOCK_STREAM, 0); // 创建一个套接字
 	if (sockfd < 0){
-		printf("crate sockfd failed\n");
+		printf("create sockfd faild\n");
 		return -1;
 	}
-	struct sockaddr_in serv_addr;
+	printf("create sockfd suss\n");
+
+	memset(&serv_addr, 0, sizeof(serv_addr)); // 一般是存储地址和ip 用于信息的显示及存储使用
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port); // 服务端口号
-	serv_addr.sin_addr.s_addr = inet_addr(ip); // 服务ip inet_addr用于IPv4的ip转换 即十进制转二进制
-	if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
-		printf("connect failed\n");
+	serv_addr.sin_port = htons(port); // 转换为网络字节序，即大端模式
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 将主机的无符号长整形数转换为网络字节序 INADDR_ANY就是0.0.0.0 表示不确定地址 或任意地址 
+	if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+		printf("bind sockfd faild\n");
 		return -1;
 	}
-	printf("connect success \n");
+	printf("bind sockfd suss\n");
+	listen(sockfd, 1024);
+	len = sizeof(serv_addr);
+
+	pthread_t connThreadId;
+	if( pthread_create(&connThreadId, NULL, getConn, NULL) != 0 ){
+			printf("create connect thread faild\n");
+			exit(0);
+	}
+	pthread_detach(connThreadId); // 分离线程 将此线程同当前进程分离，使其成为一个独立线程。
+	
+	pthread_t sendThreadId;
+	if( pthread_create(&sendThreadId, NULL, sendMess, NULL) != 0 ){
+			printf("create send thread faild\n");
+			exit(0);
+	}
+	pthread_detach(sendThreadId);
+
+	pthread_t recvThreadId;
+	if( pthread_create(&recvThreadId, NULL, recvData, NULL) != 0 ){
+			printf("create recv thread faild\n");
+			exit(0);
+	}
+	pthread_detach(recvThreadId);
 	while(1){
 
-		/*把可读文件描述符的集合清空*/
-        FD_ZERO(&rfds);
-        /*把标准输入的文件描述符加入到集合中*/
-        FD_SET(0, &rfds);
-        maxfd = 0;
-        /*把当前连接的文件描述符加入到集合中*/
-        FD_SET(sock_cli, &rfds);
-        /*找出文件描述符集合中最大的文件描述符*/   
-        if(maxfd < sock_cli)
-            maxfd = sock_cli;
-        /*设置超时时间*/
-        tv.tv_sec = 5;
-        tv.tv_usec = 0;
-        /*等待聊天*/
-        retval = select(maxfd+1, &rfds, NULL, NULL, &tv);
-        if(retval == -1)
-        {
-            printf("select出错，客户端程序退出\n");
-            break;
-        }
-        else if(retval == 0)
-        {
-            printf("客户端没有任何输入信息，并且服务器也没有信息到来，waiting...\n");
-            continue;
-        }
-        else
-        {
-            /*服务器发来了消息*/
-            if(FD_ISSET(sock_cli,&rfds))
-            {
-                char recvbuf[BUFFER_SIZE];
-                int len;
-                len = recv(sock_cli, recvbuf, sizeof(recvbuf),0);
-                printf("recv from server %s", recvbuf);
-                if (strcmp(recvbuf, "bye") ==0 ){
-                	break;
-                }
-                memset(recvbuf, 0, sizeof(recvbuf));
-            }
-            /*用户输入信息了,开始处理信息并发送*/
-            if(FD_ISSET(0, &rfds))
-            {
-                char sendbuf[BUFFER_SIZE];
-                fgets(sendbuf, sizeof(sendbuf), stdin);
-                send(sock_cli, sendbuf, strlen(sendbuf),0); //发送
-                memset(sendbuf, 0, sizeof(sendbuf));
-            }
-        }
 	}
-	printf("end connect\n");
 	close(sockfd);
 	return 0;
 }
